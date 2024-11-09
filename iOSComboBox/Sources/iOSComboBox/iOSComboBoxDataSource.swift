bf4cdf2e549e072df57df5d977efd63a32923721
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2024-11-09 13:29:53 +0100

    Comment out unused methods

--- iOSComboBox/Sources/iOSComboBox/iOSComboBoxDataSource.swift
+++ iOSComboBox/Sources/iOSComboBox/iOSComboBoxDataSource.swift
@@ -11,8 +11,8 @@ import UIKit
     
     @objc @MainActor optional func numberOfItems(in comboBox: iOSComboBox) -> Int
     @objc @MainActor optional func comboBox(_ comboBox: iOSComboBox, objectValueForItemAt index: Int) -> Any?
-    @objc @MainActor optional func comboBox(_ comboBox: iOSComboBox, indexOfItemWithStringValue string: String) -> Int
-    @objc @MainActor optional func comboBox(_ comboBox: iOSComboBox, completedString string: String) -> String?
+    //@objc @MainActor optional func comboBox(_ comboBox: iOSComboBox, indexOfItemWithStringValue string: String) -> Int
+    //@objc @MainActor optional func comboBox(_ comboBox: iOSComboBox, completedString string: String) -> String?
     
     @objc @MainActor optional func comboBox(_ comboBox: iOSComboBox, cellProvider: UITableViewCellProvider, forRowAt index: Int) -> UITableViewCell
     @objc @MainActor optional func comboBox(_ comboBox: iOSComboBox, commit editingStyle: UITableViewCell.EditingStyle, forRowAt index: Int)
